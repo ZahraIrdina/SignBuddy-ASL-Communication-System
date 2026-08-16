dc89451bb699edeab2516f56d51ba97b37374f87
Zahra Irdina Binti Issam <zahrairdina04@gmail.com>
2026-08-16 23:49:15 +0800

    Add files via upload

---
+++ mobile_apps/lib/utils/app_routes.dart
@@ -0,0 +1,20 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String home = '/home';
+
+  static const String deafToHearing = '/deaf-to-hearing';
+  static const String hearingToDeaf = '/hearing-to-deaf';
+
+  static const String deafResult = '/deaf-result';
+  static const String hearingResult = '/hearing-result';
+
+  static const String signVideoOutput = '/sign-video-output';
+  static const String alphabetOutput = '/alphabet-output';
+
+  static const String about = '/about';
+  static const String phrases = '/phrases';
+  static const String alphabet = '/alphabet';
+  static const String history = '/history';
+  static const String settings = '/settings';
+}
---
+++ mobile_apps/lib/utils/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const quickPhrases = ['Hello','Thank You','Help','Water','Toilet','Yes','No','I Love You','Good Morning'];
+}
