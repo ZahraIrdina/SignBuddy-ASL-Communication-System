60d755411e0287ebe589febcf9f3b0a621854550
Zahra Irdina Binti Issam <zahrairdina04@gmail.com>
2026-08-16 23:48:50 +0800

    Add AppColors class for color constants

---
+++ mobile_apps/lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const teal = Color(0xFF00A88F);
+  static const tealDark = Color(0xFF00897B);
+  static const blue = Color(0xFF1976D2);
+  static const purple = Color(0xFF6C3BDB);
+  static const bg = Color(0xFFF8FAFC);
+  static const text = Color(0xFF111827);
+  static const muted = Color(0xFF6B7280);
+  static const line = Color(0xFFE5E7EB);
+}
